2e4a7391d7327e58a6977f47686845cd57ec1cd7
dikshantmanwar <125083315+dikshantmanwar@users.noreply.github.com>
2023-03-02 16:40:58 +0530

    Update Interview_prog.py

--- Interview_prog.py
+++ Interview_prog.py
@@ -1,7 +1,19 @@
+# alist=[67,34,12,343,23,2]
+# n=len(alist)
+# for i in range(n):
+#     for j in range(i+1,n):
+#         if alist[i]>alist[j]:
+#             alist[i],alist[j]=alist[j],alist[i]
+# print(alist)
+
+##-----------------------------------
 alist=[67,34,12,343,23,2]
-n=len(alist)
-for i in range(n):
-    for j in range(i+1,n):
-        if alist[i]>alist[j]:
-            alist[i],alist[j]=alist[j],alist[i]
-print(alist)
+ln=sln=alist[0]
+for i in alist:
+    if i > ln:
+        ln=i
+for i in alist:
+    if i>sln and i !=ln:
+        sln=i
+print(sln)
+
